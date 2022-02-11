d825ed5b6418f6d97d5cedf62247191c7761584c
Manas Rajani <manasrajani12@gmail.com>
2022-02-11 11:32:19 +0530

    First Commit xD

---
+++ AdminMasterPage.master.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class AdminMasterPage : System.Web.UI.MasterPage
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        if(Session["email"].Equals(string.Empty))
+        {
+            Response.Redirect("~/Login.aspx");
+        }
+    }
+    protected void LinkButton1_Click(object sender, EventArgs e)
+    {
+        Session["email"] = string.Empty;
+        Response.Redirect("~/Login.aspx");
+    }
+}
